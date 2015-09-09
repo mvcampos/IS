efdadea90130579fc028e22b3c96a33da75b1580
11116060 <11116060@ESTB80704.unibh.edu>
2015-09-08 22:26:18 -0300

    commit

---
+++ pessoas/src/br/unibh/pessoas/persistencia/PessoaJuridicaDAO.java
@@ -0,0 +1,39 @@
+package br.unibh.pessoas.persistencia;
+
+import java.util.List;
+
+import br.unibh.pessoas.entidades.PessoaJuridica;
+
+public class PessoaJuridicaDAO implements DAO<PessoaJuridica, Long> {
+
+	@Override
+	public PessoaJuridica find(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(PessoaJuridica t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(PessoaJuridica t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(PessoaJuridica t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<PessoaJuridica> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
